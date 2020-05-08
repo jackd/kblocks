f98a9416ed6ea58ac4baadd656275c504f1ede6e
Dominic Jack <thedomjack@gmail.com>
2020-05-08 13:21:34 +1000

    removed buggy wrappers

--- kblocks/extras/layers/__init__.py
+++ kblocks/extras/layers/__init__.py
@@ -1,17 +1,11 @@
-from .bias_add import BiasAdd
-from .scale import Scale
-from .scale import ZeroInit
-from .denormalize import Denormalization
-from .wrapper import as_lambda
-from .wrapper import wrap_as_lambda
-from . import wrapper
+from kblocks.extras.layers.bias_add import BiasAdd
+from kblocks.extras.layers.scale import Scale, ZeroInit
+from kblocks.extras.layers.scale import ZeroInit
+from kblocks.extras.layers.denormalize import Denormalization
 
 __all__ = [
     "BiasAdd",
     "Denormalization",
     "Scale",
     "ZeroInit",
-    "as_lambda",
-    "wrap_as_lambda",
-    "wrapper",
 ]
