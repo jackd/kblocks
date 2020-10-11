8c88847b9da570b2cf0f41361f3203ac19a1e0d1
Dominic Jack <thedomjack@gmail.com>
2020-10-11 12:09:09 +1000

    added gc

--- kblocks/extras/callbacks/__init__.py
+++ kblocks/extras/callbacks/__init__.py
@@ -1,10 +1,12 @@
 from kblocks.extras.callbacks.checkpoint import CheckpointCallback
+from kblocks.extras.callbacks.gargbage_collector import GarbageCollector
 from kblocks.extras.callbacks.log_updater import LogUpdater, logged_value
 from kblocks.extras.callbacks.logger import AbslLogger
 
 __all__ = [
-    "CheckpointCallback",
     "AbslLogger",
+    "CheckpointCallback",
+    "GarbageCollector",
     "LogUpdater",
     "logged_value",
 ]
